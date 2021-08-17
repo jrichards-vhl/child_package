7940f8d1bac30353c6f86561cb60cbde71f09555
jrichards-vhl <jrichards@vistahigherlearning.com>
2021-08-17 23:34:08 +0000

    bump to v1.0.6

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
+/**
+ * Logs a hello message to the console.
+ */
 export function sayHello() {
-  console.log('hello from child package! v1.0.5');
+  console.log('hello from child package! v1.0.10');
 }
+
+// export {addNumbers} from './add_numbers';
 
