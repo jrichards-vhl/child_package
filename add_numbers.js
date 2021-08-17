897364e5038d2fc348593bdfa79c02b899b929f1
jrichards-vhl <jrichards@vistahigherlearning.com>
2021-08-17 23:34:08 +0000

    bump to v1.0.6

---
+++ add_numbers.js
@@ -0,0 +1,12 @@
+// @ts-check
+
+/**
+ * Adds two numbers.
+ * @param {number} first - The first number to add.
+ * @param {number} second - The second number to add.
+ * @returns {number} The sum of both numbers.
+ */
+export function addNumbers(first, second) {
+  return first + second;
+}
+
